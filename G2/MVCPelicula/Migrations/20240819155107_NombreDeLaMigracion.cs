fe479b4fa11d7af348d7245f97cd85ab5765617b
Eduardo Mancia <40743410+EduardMancia48@users.noreply.github.com>
2024-08-21 23:15:51 -0600

    Cambios guia5

---
+++ G2/MVCPelicula/Migrations/20240819155107_NombreDeLaMigracion.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCPelicula.Migrations
+{
+    /// <inheritdoc />
+    public partial class NombreDeLaMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Titulo",
+                table: "Peliculas",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Titulo",
+                table: "Peliculas",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+        }
+    }
+}
--- G2/MVCPelicula/Migrations/20240819155423_Inicio.Designer.cs
+++ G2/MVCPelicula/Migrations/20240819155423_Inicio.Designer.cs
@@ -4,6 +4,7 @@ using MVCPelicula.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MVCPelicula.Migrations
 {
     [DbContext(typeof(PeliculasDBContext))]
-    partial class PeliculasDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240819155423_Inicio")]
+    partial class Inicio
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
